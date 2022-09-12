7fd438f1e48a5e1cba6078cb59441d787e0acf51
roopapa <rooria24@gmail.com>
2022-09-13 00:08:15 +0900

    [feature]追従カメラを実装する
    close #18 プレイヤーを追従するカメラを実装

---
+++ Assets/Rise/Scenes/MainGame/Scripts/FollowCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField, Header("Player")]
+    private GameObject m_playerObj;
+
+    private void Update()
+    {
+        MoveCamera();
+    }
+
+    /// <summary>
+    /// カメラを移動する
+    /// </summary>
+    private void MoveCamera()
+    {
+        var playerPos = m_playerObj.transform.position;
+        transform.position = new Vector3(playerPos.x, transform.position.y, transform.position.z);
+    }
+}
