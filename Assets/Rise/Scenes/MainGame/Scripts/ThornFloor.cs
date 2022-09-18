f806f6f434bf64251a16ccca187758419eded3d1
roopapa <rooria24@gmail.com>
2022-09-18 19:17:26 +0900

    [feature]トゲ床用スクリプトThornFloor.csを追加
    プレイヤーがトゲ床に触れると死んでトゲ無効スキルを継承する機能を実装

---
+++ Assets/Rise/Scenes/MainGame/Scripts/ThornFloor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class ThornFloor : Gimmick
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            var player = collision.GetComponent<Player>();
+            if (!player.IsThornDisablementEnabled && player != null)
+            {
+                ExecutePlayerKillManager(player);
+            }
+        }
+    }
+
+    public override void ExecutePlayerKillManager(Player player)
+    {
+        var playerKillManager = new PlayerKillManager(this, player);
+        playerKillManager.PlayerKill(m_skill);
+    }
+
+    public override void PlayerKill(Player player)
+    {
+        Destroy(player.gameObject);
+    }
+}
