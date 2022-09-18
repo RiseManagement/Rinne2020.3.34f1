625178ede023620a024c179281824cec8639ec07
roopapa <rooria24@gmail.com>
2022-09-18 17:19:17 +0900

    [feature]SkillDBEntityアセットのインスペクターにスキル継承状態をリセットするボタンを追加
    close #38 継承状態が残っているとテスト時の効率が悪いため一括でスキルの継承状態をリセットできるボタンを追加

--- Assets/Rise/Scenes/MainGame/Scripts/SkillDBEditor.cs
+++ Assets/Rise/Scenes/MainGame/Scripts/SkillDBEditor.cs
@@ -1,18 +1,18 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class SkillDBEditor : MonoBehaviour
+[CustomEditor(typeof(SkillDBEntity))]
+public class SkillDBEditor : Editor
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void OnInspectorGUI()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        base.OnInspectorGUI();
+        var skillDBEntity = target as SkillDBEntity;
+        if (GUILayout.Button("継承状態リセット"))
+        {
+            skillDBEntity.ResetInherited();
+        }
     }
 }
