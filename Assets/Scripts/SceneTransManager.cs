7e3a9fe69fd09bb573015dc78b1c408705b1137b
roopapa <rooria24@gmail.com>
2022-09-22 22:34:56 +0900

    [feature]シーン遷移管理用クラスを作成する
    シーン遷移を実行するためにシーン遷移管理用クラスを追加

---
+++ Assets/Scripts/SceneTransManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransManager
+{
+    /// <summary>
+    /// メインゲームシーンに遷移する
+    /// </summary>
+    public static void TransToMainGame()
+    {
+        // TODO: メインゲームシーンをロードする
+    }
+
+    /// <summary>
+    /// ゲームオーバーシーンに遷移する
+    /// </summary>
+    public static void TransToGameOver()
+    {
+        // TODO: ゲームオーバーシーンをロードする
+    }
+}
