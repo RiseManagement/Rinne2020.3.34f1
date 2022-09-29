7f4d4ff8dd18aec703bbeb4a39c45e9f936d7462
roopapa <rooria24@gmail.com>
2022-09-30 01:39:22 +0900

    [feature]MainGameSceneでプレイヤーが死ぬとSkillSceneに遷移する機能を追加
    プレイヤーが死んでもSkillSceneに遷移しなかったため実装

---
+++ Assets/Scripts/SkillSceneManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillSceneManager : MonoBehaviour
+{
+    [SerializeField, Header("トゲ無効スキル")]
+    private Skill m_thornDisablementSkill;
+    [SerializeField, Header("ジャンプ力強化スキル")]
+    private Skill m_jumpPowerEnhancementSkill;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log($"トゲ無効スキルの継承状態がリセットされていないか:{m_thornDisablementSkill.IsInherited}");
+        Debug.Log($"ジャンプ力強化スキルの継承状態がリセットされていないか:{m_jumpPowerEnhancementSkill.IsInherited}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneTransManager.TransToMainGame();
+        }
+    }
+}
