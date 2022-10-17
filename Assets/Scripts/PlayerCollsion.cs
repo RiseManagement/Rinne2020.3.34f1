ee3a14c8b769011c43079ca0849f8d4bfcaf031f
hamatyan <hiroakipokemon@gmail.com>
2022-10-17 23:22:43 +0900

    プレイヤーと敵の当たり判定を追加

---
+++ Assets/Scripts/PlayerCollsion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollsion : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log(collision.gameObject.name + "と判定した");
+            //ここからプレイヤーに起きることを記述
+        }
+    }
+}
