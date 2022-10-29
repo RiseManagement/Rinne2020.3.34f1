ae3bd1d04655c93ad846355c542a7a5374aaee3c
roopapa <rooria24@gmail.com>
2022-10-30 02:51:18 +0900

    [feature]ゲームクリア画面を作成
    close #114

---
+++ Assets/Scripts/GameClearManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameClearManager : MonoBehaviour
+{
+    /// <summary>
+    /// タイトルに戻る
+    /// </summary>
+    public void BackToTitle()
+    {
+        SceneTransManager.TransToTitle();
+    }
+}
