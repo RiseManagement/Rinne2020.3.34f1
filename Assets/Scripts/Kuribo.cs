0b213ef82ff91d6db7de07577e4f517b548aa48d
roopapa <rooria24@gmail.com>
2022-10-29 19:56:58 +0900

    [feature]クリボーに触れるとプレイヤーが死ぬ機能を実装
    クリボーに触れてもプレイヤーが死ぬ機能がなかったため実装した

---
+++ Assets/Scripts/Kuribo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kuribo : Enemy
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            var player = collision.GetComponent<Player>();
+
+            if (player != null)
+            {
+                ExecutePlayerKillManager(player);
+            }
+        }
+    }
+
+    public override void ExecutePlayerKillManager(Player player)
+    {
+        var playerKillManager = new PlayerKillManager(player);
+        playerKillManager.PlayerKill(this, m_skill);
+    }
+
+    public override void PlayerKill(Player player)
+    {
+        Destroy(player.gameObject);
+    }
+}
