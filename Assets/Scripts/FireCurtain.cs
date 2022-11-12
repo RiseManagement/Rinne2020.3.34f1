1d268659c84a7f9d444e1f8c4da094445f2f1434
roopapa <rooria24@gmail.com>
2022-11-12 16:58:22 +0900

    [feature]プレイヤーが火に耐えるカウントを衝突中の経過時間から衝突した回数に変更

--- Assets/Scripts/FireCurtain.cs
+++ Assets/Scripts/FireCurtain.cs
@@ -11,28 +11,15 @@ public class FireCurtain : Gimmick
             var player = collision.GetComponent<Player>();
 
             // playerがnullじゃない、かつ焼死耐性スキルが継承されていなければ実行
-            if (player != null && !player.IsBurnoutResistanceEnabled)
+            if (player != null)
             {
-                ExecutePlayerKillManager(player);
-            }
-        }
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            var player = collision.GetComponent<Player>();
-
-            // playerがnullじゃない、かつ焼死耐性スキルが継承されていれば実行
-            if (player != null && player.IsBurnoutResistanceEnabled)
-            {
-                // プレイヤーが火に耐えられる時間を減らす
-                player.FireCount -= 1f * Time.deltaTime;
-
-                if (player.FireCount <= 0f)
+                if (player.EnduranceCount == 0)
                 {
                     ExecutePlayerKillManager(player);
+                }
+                else
+                {
+                    player.EnduranceCount -= 1;
                 }
             }
         }
