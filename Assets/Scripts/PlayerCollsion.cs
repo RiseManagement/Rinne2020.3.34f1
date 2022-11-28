82fd159514156eb3aaea534f13f9276957caedff
hamatyan <hiroakipokemon@gmail.com>
2022-11-28 23:10:34 +0900

    移動する床機能追加

--- Assets/Scripts/PlayerCollsion.cs
+++ Assets/Scripts/PlayerCollsion.cs
@@ -12,4 +12,22 @@ public class PlayerCollsion : MonoBehaviour
             //ここからプレイヤーに起きることを記述
         }
     }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (transform.parent == null && col.gameObject.name == "MoveFloor")
+        {
+            var emptyObject = new GameObject();
+            emptyObject.transform.parent = col.gameObject.transform;
+            transform.parent = emptyObject.transform;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (transform.parent != null && col.gameObject.name == "MoveFloor")
+        {
+            transform.parent = null;
+        }
+    }
 }
