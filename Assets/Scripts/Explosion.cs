4802a1cfee3d3d407f800b8460fd8c20dedad400
roopapa <rooria24@gmail.com>
2022-12-03 17:32:12 +0900

    [feature]爆弾花が爆発する機能を実装

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 爆発
+/// </summary>
+public class Explosion : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
