17ffe2d92a801de5df52d01465e1236166185a0e
roopapa <rooria24@gmail.com>
2023-02-04 13:42:28 +0900

    [feature]チュートリアルキャラがプレイヤーを追従する機能を実装
    チュートリアルキャラがプレイヤーと一緒に行動するためプレイヤーを追従する機能を実装した

---
+++ Assets/Scripts/TutorialCharacterController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialCharacterController : MonoBehaviour
+{
+    [SerializeField, Header("Player")]
+    private GameObject m_playerObj;
+
+    private void Update()
+    {
+        if (m_playerObj != null)
+        {
+            Move();
+        }
+    }
+
+    /// <summary>
+    /// 移動する(プレイヤーを追従する)
+    /// </summary>
+    private void Move()
+    {
+        var playerPos = m_playerObj.transform.position;
+            transform.position = new Vector3(playerPos.x + 0.45f, playerPos.y + 0.45f, transform.position.z);
+    }
+}
