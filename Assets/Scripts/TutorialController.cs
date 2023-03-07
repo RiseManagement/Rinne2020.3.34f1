bdbfb221a4d5c997f7ad1b55455c5518cdf791bb
hamatyan <hiroakipokemon@gmail.com>
2023-03-08 00:08:31 +0900

    追加：チュートリアル説明表示位置

---
+++ Assets/Scripts/TutorialController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour
+{
+    //チュートリアルキャンバス
+    public GameObject tutorialCanvasObj;
+
+    //チュートリアルキャラ
+    public GameObject tutorialCharacterObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetDrawPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tutorialCanvasObj.activeSelf)
+        {
+            Debug.Log("表示");
+        }
+        else
+        {
+            Debug.Log("非表示");
+        }
+    }
+
+    /// <summary>
+    /// チュートリアル説明表示
+    /// </summary>
+    void TutorialExplanationActive()
+    {
+        tutorialCanvasObj.SetActive(true);
+    }
+
+    /// <summary>
+    /// チュートリアル説明非表示
+    /// </summary>
+    void TutorialExplanationNotActive()
+    {
+        tutorialCanvasObj.SetActive(false);
+    }
+
+    /// <summary>
+    /// チュートリアルCanvasの座標セット
+    /// </summary>
+    void SetDrawPos()
+    {
+        var FrameRect = tutorialCanvasObj.transform.GetChild(0).GetComponent<RectTransform>();
+        var framerectWidth = FrameRect.rect.width;
+        var framerectHeght = FrameRect.rect.height;
+
+        FrameRect.anchoredPosition = new Vector2(
+            tutorialCharacterObj.transform.position.x + framerectWidth / 2.5f,
+            tutorialCharacterObj.transform.position.y + framerectHeght);
+    }
+}
