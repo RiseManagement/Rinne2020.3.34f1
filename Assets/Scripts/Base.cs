d872d5ed62dc991944b7cdccaef4cbfde0696f08
roopapa <rooria24@gmail.com>
2023-03-18 03:06:24 +0900

    [feature]拠点のスクリプトを追加

---
+++ Assets/Scripts/Base.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Base : MonoBehaviour
+{
+    [SerializeField, Header("チュートリアルキャラ用ソケット")]
+    private GameObject m_spCharaSocket;
+
+    /// <summary>
+    /// スキル入れ替えシーンに遷移する
+    /// </summary>
+    public void TransToSkillSwapScene()
+    {
+        SceneTransManager.TransToSkillSwap();
+    }
+}
