cbf4127efec8566905a6d57d6b388b219f8ce01f
roopapa <rooria24@gmail.com>
2023-05-06 22:06:40 +0900

    [feature]トラップエリアにギミックとエネミーを配置した
    トラップエリアにSteelTrap(トラバサミ)とバクダンバナを配置した

---
+++ Assets/Scripts/SteelTrap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteelTrap : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            // プレイヤースクリプトを取得
+            var player = collision.GetComponent<Player>();
+            if (player != null)
+            {
+                Destroy(player.gameObject);
+            }
+        }
+    }
+}
