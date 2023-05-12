ce4620eae45b6243d3c4ebc5261d1836b4d9e51d
roopapa <rooria24@gmail.com>
2023-05-13 05:14:30 +0900

    [feature]ステージクリアオブジェクトを実装

--- Assets/Scripts/StageClear.cs
+++ Assets/Scripts/StageClear.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class StageClear : MonoBehaviour
 {
+    [SerializeField, Header("遷移先のステージ番号")]
+    private int m_nextStageNumber;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            SceneTransManager.TransToStageClear();
+            SceneTransManager.TransToStage(m_nextStageNumber);
         }
     }
 }
