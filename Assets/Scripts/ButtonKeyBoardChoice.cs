8374ac5ca25f49f5988544d7e3ac55181538e795
LuGeNe0N <lie10210@gmail.com>
2023-05-26 09:02:42 +0900

    RINNE-75 キーボードでの選択仮実装

---
+++ Assets/Scripts/ButtonKeyBoardChoice.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonKeyBoardChoice : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Button button;
+    void Start()
+    {
+        button = GameObject.Find("MenuCanvas/GameStartButton").GetComponent<Button>();
+        //ボタンが選択された状態になる
+        button.Select();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
